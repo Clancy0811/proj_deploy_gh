35b1a2abbe98f52ade1b8391cd0bea2531811c2d
Zixun <zixun811@gmail.com>
2026-01-30 12:51:55 +1100

    first commit

---
+++ src/Square.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Square = ({ colorValue, hexValue, isDarkText }) => {
+  return (
+    <section
+        className='square'
+        style=  {{
+                    backgroundColor: colorValue,
+                    color: isDarkText ? "#fff" : "#000"
+                }}
+    >
+        <p>{ colorValue ? colorValue : "Empty Value" }</p>
+        <p>{hexValue ? hexValue : null}</p>
+    </section>
+  )
+}
+
+export default Square
